83c9e3ac2e8f7135d5fc58aa03719023a2617c28
David Adriel <95537646+davidadriel64@users.noreply.github.com>
2023-01-13 20:03:38 -0300

    Error deploy proyecto final coder

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,10 +5,10 @@ const Footer = () => {
         <div className="container">
         <footer className="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
           <div className="col-md-4 d-flex align-items-center">
-            <span className="mb-3 mb-md-0 text-muted">© 2022 Company, Inc</span>
+            <span className="mb-3 mb-md-0 text-muted">© 2022 Tienda Ya</span>
           </div>
           <ul className="nav col-md-4 justify-content-end list-unstyled d-flex">
-            <li className="ms-3"><a className="text-muted" href="#">xxxx</a></li>
+            <li className="ms-3"><p className="text-muted" >App demo</p></li>
            </ul>
         </footer>
       </div>
